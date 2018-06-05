304f80858768b4804ff73b745c1b59a2a123913f
Dan Kang <dankang27@gmail.com>
2018-06-04 21:31:18 -0400

    add api call

--- src/App.js
+++ src/App.js
@@ -2,18 +2,62 @@ import React, { Component } from "react";
 import "./App.css";
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      content: {},
+      meta: {}
+    };
+  }
+
   componentDidMount() {
-    this.content = fetch("http://example.com/movies.json").then((response) => {
-      return response.json();
-    }).then((response) => {
-      console.log(response.json());
+    fetch("http://srx.pubcdn.net/pdata.php?v=2&id=1940&x=6")
+      .then(response => {
+        return response.json();
+      })
+      .then(data => {
+        this.setState({
+          content: data.c,
+          meta: data.m
+        });
+      });
+  }
+
+  allContent() {
+    this.state.content.map(item => {
+      return (
+        <section>
+          item
+        </section>
+      );
     });
   }
 
+
+
   render() {
+    console.log(this.state.content);
+    console.log(this.state.meta);
+
     return (
       <div className="App">
-        
+        {
+          (
+            Object.values(this.state.content).length > 0 ? this.state.meta.i : ""
+          )
+        }
+        {
+          (Object.values(this.state.content).length > 0 ?
+            this.state.content.map((el) => {
+              return (
+                <section>
+                  <img src={el.i}/>
+                  {el.t}
+                </section>
+              );
+            }) : "ok"
+          )
+        }
       </div>
     );
   }
